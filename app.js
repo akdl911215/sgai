ec856438a51951d583b36401a890c54f3d304689
akdl911215 <akdl911215@naver.com>
2023-10-26 20:11:51 +0900

    sg project modi

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const accessKey = "36196afa-be40-48cb-a2df-301a3e325a2c";
 const languageCode = "korean";
 
 const express = require("express");
+const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const hostname = "127.0.0.1";
+
+const multipart = require("connect-multiparty");
+app.use(multipart());
+
 // app.use(cors());
 app.listen(PORT, hostname, () => {
   console.log(`Server running at http://${hostname}:${PORT}`);
@@ -22,43 +27,54 @@ app.get("/example", (req, res) => {
 
 app.post("/ai", async (req, res, next) => {
   //
-  // console.log("req : ", req);
-  const audioFilePath =
-    req.body || "/Users/ijeonghyeon/Downloads/KOR_F_RM0769FLJH0325.pcm";
-  console.log("audioFilePath : ", audioFilePath);
-  //
-  const audioData = fs.readFileSync(audioFilePath);
-
-  const requestJson = {
-    argument: {
-      language_code: languageCode,
-      audio: audioData.toString("base64"),
-    },
-  };
-
-  const request = require("request");
-  const options = {
-    url: openApiURL,
-    body: JSON.stringify(requestJson),
-    headers: { "Content-Type": "application/json", Authorization: accessKey },
-  };
-
-  request.post(options, function (error, response, body) {
-    console.log("responseCode = " + response.statusCode);
-    console.log("responseBody = " + body);
-
-    if (error) {
-      console.log(error);
-      throw new Error(error);
+  try {
+    console.log("req.files.filename : ", req.files.filename.path);
+    const audioFilePath = req.files.filename.path;
+    console.log("audioFilePath : ", audioFilePath);
+    if (!audioFilePath) {
+      res.json({ error: "notfound path" });
+      throw new Error("notfound path");
     }
 
-    const object = JSON.parse(body);
+    const audioData = fs.readFileSync(audioFilePath);
 
-    const strSplit = object?.return_object?.recognized?.split(" ");
+    const requestJson = {
+      argument: {
+        language_code: languageCode,
+        audio: audioData.toString("base64"),
+      },
+    };
 
-    res.json({
-      token: object?.return_object?.recognized,
-      count: strSplit.length,
+    const request = require("request");
+    const options = {
+      url: openApiURL,
+      body: JSON.stringify(requestJson),
+      headers: { "Content-Type": "application/json", Authorization: accessKey },
+    };
+
+    request.post(options, function (error, response, body) {
+      console.log("responseCode = " + response.statusCode);
+      console.log("responseBody = " + body);
+
+      if (error) {
+        console.log(error);
+        throw new Error(error);
+      }
+
+      const object = JSON.parse(body);
+
+      const strSplit = object?.return_object?.recognized?.split(" ");
+
+      res.json({
+        token: object?.return_object?.recognized,
+        count: strSplit.length,
+      });
     });
-  });
+  } catch (e) {
+    console.log("error : ", e);
+  }
 });
+
+const files = {
+  file: formdata,
+};
